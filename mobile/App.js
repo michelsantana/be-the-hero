3647985d096b618cd31037101187d25d10a72b2e
Michel Santana de Oliveira Sampaio <mich3l.santana0@gmail.com>
2020-03-29 00:29:51 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+//div, span, h1
+//div -> view
+//span, p, h1 -> text
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
